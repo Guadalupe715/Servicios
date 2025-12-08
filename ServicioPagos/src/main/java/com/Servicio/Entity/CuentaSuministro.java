8a08344f72b56e73019c76023813588bd1e4b8c8
Guadalupe715 <rolandoguadalupe715@gmail.com>
2025-12-07 22:28:36 -0500

    Entidad Cuenta de Servicios

--- ServicioPagos/src/main/java/com/Servicio/Entity/CuentaSuministro.java
+++ ServicioPagos/src/main/java/com/Servicio/Entity/CuentaSuministro.java
@@ -1,5 +1,23 @@
 package com.Servicio.Entity;
 
-public class CuentaSuministro {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CuentaSuministro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idCuentaSuministro;
+    private  String codigoSuministro;
+    private String nombreCliente;
+    private double monto;
+    private String telefono;
 }
