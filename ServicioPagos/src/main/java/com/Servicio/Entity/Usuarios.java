72620daaee21995ae8c46e8f409393f778225bc7
Guadalupe715 <rolandoguadalupe715@gmail.com>
2025-12-07 22:23:39 -0500

    Entidad Usuario

--- ServicioPagos/src/main/java/com/Servicio/Entity/Usuarios.java
+++ ServicioPagos/src/main/java/com/Servicio/Entity/Usuarios.java
@@ -1,4 +1,18 @@
 package com.Servicio.Entity;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Usuarios {
+   private Integer idUsuario;
+    private String nombre;
+    private String usuario;
+    private String email;
+    private String rol;
 }
